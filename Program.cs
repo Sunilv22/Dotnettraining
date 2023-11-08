54da8d4e8ef494f8d9e63a2dc8728e05220cdf07
sunil <Sunilv22@gmail.com>
2023-11-09 00:24:57 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace DOTNETCOREDEMO
+{
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            //for (int i = 0; i < cars.Length; i++)
+            //{
+            //    Console.WriteLine(cars[i]);
+            //}
+            //string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            //Console.WriteLine(cars.Length);
+            //string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            //Console.WriteLine(cars[0]);
+            //string x = "10";
+            //string y = "20";
+            //string z = x + y;
+            //Console.WriteLine(z);
+            //string firstName = "John ";
+            //string lastName = "Doe";
+            //string name = string.Concat(firstName, lastName);
+            //Console.WriteLine(name);
+            //string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            //Console.WriteLine("The length of the txt string is: " + txt.Length);
+            //string n = "sunil";
+            //Console.WriteLine(n.ToLower());
+            //Console.WriteLine(n.ToUpper());
+            //int x = 5;
+            //int y = 3;
+            //Console.WriteLine(x > y);
+            ////int x = 10;
+            //x += 5;
+            //Console.WriteLine(x);
+            //int sum1 = 100 + 50;        // 150 (100 + 50)
+            //int sum2 = sum1 + 250;      // 400 (150 + 250)
+            //int sum3 = sum2 + sum2;
+            //Console.WriteLine(sum3);
+            // demo d = new demo();
+            // Implicit casting is done automatically when passing a smaller size type to a larger size type:
+            //int myInt = 9;
+            //double myDouble = myInt;       // Automatic casting: int to double
+            //Console.WriteLine(myInt);      // Outputs 9
+            //Console.WriteLine(myDouble);
+            // Explicit casting must be done manually by placing the type in parentheses in front of the value
+            //double myDouble = 9.78;
+            //int myInt = (int)myDouble;    // Manual casting: double to int
+
+            //Console.WriteLine(myDouble);   // Outputs 9.78
+            //Console.WriteLine(myInt);
+            //int a = 5, b = 6, c= 50;
+            //Console.WriteLine(a + b + c);
+            //int x = 5;
+            //int y = 6;
+            //Console.WriteLine(x + y);
+            //string firstName = "John ";
+            //string lastName = "Doe";
+            //string fullName = firstName + lastName;
+            //Console.WriteLine(fullName);
+            //string name = "John";
+            //int myNum;
+            //myNum = 20;
+            //int b = 5;
+            //double myDoubleNum = 5.99D;
+            //char myLetter = 'D';
+            //bool myBool = true;
+            //string myText = "Hello";
+            //Console.WriteLine(myDoubleNum);
+            //Console.WriteLine(myBool);
+            //Console.WriteLine(myLetter);
+            //Console.WriteLine(myText);
+
+        }
+    }
+    
+}
---
+++ demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNETCOREDEMO
+{
+     class demo
+    {
+        string name = "John";
+        string firstName = "John ";
+        string lastName = "Doe";
+       // string fullName = firstName + lastName;
+
+    }
+}
