78c5dfaf645c923e66889a8d99ff8496a3663605
sunil <Sunilv22@gmail.com>
2023-11-13 23:54:31 +0530

    fourthcommit

---
+++ methodoverriding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNETCOREDEMO
+{
+    public class Loadparent
+    {
+       public void Show()
+        {
+            Console.WriteLine("parent show method is called");
+        }
+        public virtual void Test()//overridable
+        {
+            Console.WriteLine("parent test method is called");
+        }
+
+    }
+    public class LoadChild:Loadparent 
+    {
+        public override void Test()
+        {
+            Console.WriteLine("child test method is called");
+        }
+        public void Show(int i)
+        {
+            Console.WriteLine("child show method is called");
+        }
+    }
+}
