2b31595a3e69160994055c4ecc9ff4cd2edda946
sunil <Sunilv22@gmail.com>
2023-11-13 23:53:06 +0530

    polymorphisum

---
+++ Inheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNETCOREDEMO
+{
+    public class employe
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string Email { get; set; }   
+        public void printfuname()
+        {
+            Console.WriteLine(firstname + lastname);
+        }
+
+    }
+    public class Fulltimeemploye:employe
+    {
+        public float yearlysalary { get; set; }
+        
+    }
+    public class Parttimeemploye:Fulltimeemploye
+    {
+        public float hourlyrate { get; set; }
+    }
+}
---
+++ methodoverloading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNETCOREDEMO
+{
+    public class methodoverloading
+    {
+        public void Test()
+        {
+            Console.WriteLine("1stmethod");
+        }
+        public void Test(int i)
+        {
+            Console.WriteLine("2ndmethod");
+        }
+        public void Test(string s)
+        {
+            Console.WriteLine("3ndmethod");
+        }
+        public void Test(int i,string s)
+        {
+            Console.WriteLine("4ndmethod");
+        }
+        public void Test(string s,int i)
+        {
+            Console.WriteLine("5ndmethod");
+        }
+    }
+}
